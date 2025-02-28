8d48e2ca516f2fc6fdf2a807ec1c8cc1a3d0d65f
zobayerdev <zobayer.dev@gmail.com>
2025-02-28 22:09:36 +0600

    project create

---
+++ lib/welcome_screen.dart
@@ -0,0 +1,32 @@
+import 'package:wallpaper_hub/assets_helper/app_colors.dart';
+import 'package:wallpaper_hub/assets_helper/app_images.dart';
+import 'package:flutter/material.dart';
+
+final class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primaryColor,
+      body: SizedBox(
+        height: double.infinity,
+        width: double.infinity,
+        //decoration: const BoxDecoration(color: AppColors.allPrimaryColor),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              height: 40,
+            ),
+            Image.asset(
+              AppImages.appLogo,
+              height: 170,
+              width: 170,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
